3dd49eaf185771e944cea7b6faaa1449e6ed8306
Chris <chris.reviresco@gmail.com>
2020-06-24 16:51:14 +0200

    updates fetch function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,10 @@
 var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+  // var response = fetch("https://api.github.com/users/octocat/repos");
+
+  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    console.log("inside", response);
+  });
+  console.log("outside");
 };
 
 getUserRepos();
