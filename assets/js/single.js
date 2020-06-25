d40455247acd2c614a2800104e0e9cf8eefd98ef
Chris <chris.reviresco@gmail.com>
2020-06-25 22:13:58 +0200

    added debugger

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,21 @@
+debugger;
+var displayIssues = function (issues) {
+  for (var i = 0; i < issues.length; i++) {
+    // create a linnk element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+  }
+};
+
 var getRepoIssues = function (repo) {
   console.log(repo);
   var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   fetch(apiURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        displayIssues(data);
       });
     } else {
       alert("There was a problem with your request!");
