ae8120376b7d5d4f778ba1a0e92ba95f5073ac6d
Chris <chris.reviresco@gmail.com>
2020-06-26 15:27:15 +0200

    single page repo update

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,36 @@
-debugger;
+var issueContainerEl = document.querySelector("#issues-container");
+
 var displayIssues = function (issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues";
+  }
   for (var i = 0; i < issues.length; i++) {
     // create a linnk element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
+
+    // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or a pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+    issueContainerEl.appendChild(issueEl);
   }
 };
 
@@ -23,4 +48,4 @@ var getRepoIssues = function (repo) {
   });
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues("withchristopher/csmportfolio");
