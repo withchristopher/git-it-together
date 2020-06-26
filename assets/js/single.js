2db4b282ebf338a86736fc98b6ef66f49fd674f8
Chris <chris.reviresco@gmail.com>
2020-06-26 16:07:30 +0200

    completed 30 issues listing

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -34,6 +35,19 @@ var displayIssues = function (issues) {
   }
 };
 
+var displayWarning = function (repo) {
+  // add text to warnning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
 var getRepoIssues = function (repo) {
   console.log(repo);
   var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -41,6 +55,10 @@ var getRepoIssues = function (repo) {
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+        // check if api has paginnated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -48,4 +66,4 @@ var getRepoIssues = function (repo) {
   });
 };
 
-getRepoIssues("withchristopher/csmportfolio");
+getRepoIssues("facebook/react");
